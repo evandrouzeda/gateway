61e1f77dc1efbea8e6a1e43fef08a859c5011f4b
evandrouzeda <evandrouzeda@gmail.com>
2022-03-12 18:34:09 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var http = require('http');
+const express = require('express')
+const httpProxy = require('express-http-proxy')
+const app = express()
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const helmet = require('helmet');
+
+const carteiraServiceProxy = httpProxy('http://localhost:3003');
+//const pessoasServiceProxy = httpProxy('http://localhost:3003');
+
+// Proxy request
+// rota para carteira e todos os métodos
+app.all('/carteira', (req, res, next) => {
+    carteiraServiceProxy(req, res, next);
+})
+// rota para carteira e todos os métodos com um parâmetro ID
+app.all('/carteira/:id', (req, res, next) => {
+    carteiraServiceProxy(req, res, next);
+})
+// rota para pessoas e todos os métodos
+app.all('/ativo', (req, res, next) => {
+    pessoasServiceProxy(req, res, next);
+})
+// rota para pessoas e todos os métodos com um parâmetro ID
+app.all('/ativo/:id', (req, res, next) => {
+    pessoasServiceProxy(req, res, next);
+})
+
+app.use(logger('dev'));
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+var server = http.createServer(app);
+server.listen(5000);
